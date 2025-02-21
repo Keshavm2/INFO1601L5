0a84464139e9be6f09bb47ae34c227fc65216c6b
Keshavm2 <keshavmaharaj45@gmail.com>
2025-02-21 13:56:38 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,77 @@
-let arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];// 10 elements
+//object literal
+let bob = {
+  fname: "bob",
+  lname: "smith",
+  age: 18,
+  height: 6,
+  transcript:[
+    {
+      course: 'INFO 1603',
+      grades: [ 89, 34, 67 ]
+    },
+    {
+      course: 'INFO 1601',
+      grades: [ 89, 34, 67 ]
+    }
+  ]
+};
 
-for(let i=0; i< arr.length; i++){
-  console.log(arr[i])
+let sally = {
+  fname: "sally",
+  lname: "smith",
+  age: 18,
+  height: 6,
+  transcript:[
+    {
+      course: 'INFO 1601',
+      grades: [ 100, 89, 79 ]
+    }
+  ]
+};
+
+let paul = {
+  fname: "paul",
+  lname: "smith",
+  age: 18,
+  height: 6,
+  transcript:[
+    {
+      course: 'INFO 1600',
+      grades: [ 89, 34, 67 ]
+    }
+  ]
+};
+
+
+const students = [bob, sally, paul];
+
+function getAverageGrade(student,course_code){
+
+for(let record of student.transcript){
+
+  if(record.course === course_code){
+    let sum = 0;
+  for(let grade of record.grades){
+    sum += grade;
+    }
+    return sum / record.grades.length;
+  }
 }
+return -1;
+}
+
+function getAssignmentMark(student,course,num){
+
+  for(let record of student.transcript){
+
+    if(record.course === course_code){
+      if(num >=0 && num<record.grades.length){
+        return record.grades[num];
+      }
+    }
+  return -1;
+  }
+}
+
+console.log(getAverageGrade(bob, 'INFO 1603'));
+console.log(getAssignmentMark(sally, 'INFO 1603'));
