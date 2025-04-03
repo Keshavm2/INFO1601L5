23cc584b6806c9402112c8ea38fbc66bca5bc5b7
Keshavm2 <keshavmaharaj45@gmail.com>
2025-04-03 19:58:53 -0400

    Update index.js

--- index.js
+++ index.js
@@ -48,7 +48,7 @@ const students = [bob, sally, paul];
 function getAverageGrade(student,course_code){
 
 for(let record of student.transcript){
-
+  
   if(record.course === course_code){
     let sum = 0;
   for(let grade of record.grades){
@@ -62,39 +62,31 @@ return -1;
 
 function getAssignmentMark(student,course,num){
 
-  for(let record of student.transcript){
-
-    if(record.course === course){
-      if(num >=0 && num<record.grades.length){
-        return record.grades[num];
+  for(let transcript of student.transcript){
+    if(transcript.course === course){
+        return transcript.grades[num];
       }
     }
-  }
   return -1;
 }
 
-function averageAssessment(students, courseName, assignment){
+function averageAssessment(students, courseName,assignment){
 
   let sum=0;
   let count=0;
 
   for(let student of students){
-    for(let record of student.transcript){
-      if(record.course === courseName){
-        if(assignment >=0 && assignment<record.grades.length){
-          sum = sum + record.grades[assignment];
-          count++;
-        }
-      }  
-    }
-  }
-  if(count >0){
-    return sum/count;
-  }
-  return -1;
+      let mark = getAssignmentMark(student,courseName,assignment);
+      if(mark !== -1){
+        sum += mark;
+        count++;
+      }
+        }  
+
+  return sum / count;
 }
 
 
 console.log(getAverageGrade(bob, 'INFO 1603'));
-console.log(getAssignmentMark(sally, 'INFO 1603'));
-console.log(averageAssessment(students,'INFO 1603'));
+console.log(getAssignmentMark(sally, 'INFO 1601', 2));
+console.log(averageAssessment(students,'INFO 1603', 0));
